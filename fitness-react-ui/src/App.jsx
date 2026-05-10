2082a738cb820cfa67295c5fc8f8e327a73759fd
PRIYANSHUPATEL7654 <priyanshuspatel.2003@gmail.com>
2026-05-11 02:41:40 +0530

    url updated

--- fitness-react-ui/src/App.jsx
+++ fitness-react-ui/src/App.jsx
@@ -27,7 +27,7 @@ function App() {
 
     try {
       // Replace with your backend API URL
-      const response = await fetch("/predict", {
+      const response = await fetch("http://127.0.0.1:8000/predict", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
